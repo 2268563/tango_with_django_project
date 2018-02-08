316bc4d91e33cb53334708a476437d7025bc42a9
Gordon Wells <2268563w@student.gla.ac.uk>
2018-02-08 00:24:58 +0000

    completed up to end of chapter 9

--- rango/views.py
+++ rango/views.py
@@ -36,6 +36,7 @@ def show_category(request, category_name_slug):
     
     return render(request, 'rango/category.html', context_dict)
 
+@login_required
 def add_category(request):
     form = CategoryForm()
 
@@ -51,6 +52,7 @@ def add_category(request):
 
     return render(request, 'rango/add_category.html', {'form': form})
 
+@login_required
 def add_page(request, category_name_slug):
     try:
         category = Category.objects.get(slug=category_name_slug)
@@ -107,6 +109,9 @@ def register(request):
                 'registered':registered})
 
 def user_login(request):
+    bad_details = False
+    disabled_account = False
+
     if request.method == 'POST':
         username = request.POST.get('username')
         password = request.POST.get('password')
@@ -118,16 +123,18 @@ def user_login(request):
                 login(request, user)
                 return HttpResponseRedirect(reverse('index'))
             else:
-                return HttpResponse("Your Rango account is disabled.")
+                disabled_account = True
         else:
             print("Invalid login details: {0}, {1}".format(username, password))
-            return HttpResponse("Invalid login details supplied.")
+            bad_details = True
+        return render(request, 'rango/login.html', {'bad_details':bad_details,
+            'disabled_account':disabled_account,})
     else:
         return render(request, 'rango/login.html', {})
 
 @login_required
 def restricted(request):
-    return HttpResponse("Since you're logged in, you can see this text!")
+    return render(request, 'rango/restricted.html', {})
 
 @login_required
 def user_logout(request):
